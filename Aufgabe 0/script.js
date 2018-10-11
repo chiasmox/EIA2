c21c6529ac123ba2df490be7c7886fcf94d94e21
Nastya <Nastya@DESKTOP-0KQ4TVJ.eduroam.hs-furtwangen.de>
2018-10-11 11:53:59 +0200

    No any

--- Aufgabe 0/script.js
+++ Aufgabe 0/script.js
@@ -1,10 +1,11 @@
 function begruessung() {
     var input;
     var input = prompt("Insert your name here!");
-    var node = document.getElementById("content");
-    node.innerHTML = "Hey, what's up ";
-    node.innerHTML += input;
-    node.innerHTML += "?";
+    var text;
+    text = "Hey, what's up ";
+    text += input;
+    text += "?";
+    text += document.getElementById("content");
     console.log("Hey, what's up " + input + "?");
 }
 document.addEventListener('DOMContentLoaded', begruessung);
--- Aufgabe 0/script.ts
+++ Aufgabe 0/script.ts
@@ -1,11 +1,12 @@
 function begruessung() {
 var input : string; 
 var input = prompt ("Insert your name here!");
-     var node : any = document.getElementById("content");
+     var text : string;
     
-    node.innerHTML = "Hey, what's up ";
-    node.innerHTML += input;
-    node.innerHTML += "?";
+    text = "Hey, what's up ";
+    text += input;
+    text += "?";
+    text += document.getElementById("content");
      
     console.log("Hey, what's up " +input +  "?");
     
